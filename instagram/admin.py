42b16a08f4b2628bfecd5bcccce5354f8bf04954
DaeLee <masterkorea01@naver.com>
2020-06-15 12:48:00 +0900

    media

--- instagram/admin.py
+++ instagram/admin.py
@@ -1,11 +1,13 @@
 from django.contrib import admin
 from .models import Post
+from django.utils.safestring import mark_safe
 
 
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
     list_display = [
         'id',
+        "photo_tag",
         'message',
         "message_length",
         'created_at',
@@ -15,3 +17,8 @@ class PostAdmin(admin.ModelAdmin):
     list_display_links = ['message']
     search_fields = ["message"]
     list_filter = ['is_public']
+
+    def photo_tag(self, post):
+        if post.photo:
+            return mark_safe(f'<img src="{post.photo.url}" style="width:100px;"/>')
+        return None
